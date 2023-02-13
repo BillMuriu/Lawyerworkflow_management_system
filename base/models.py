ad95a9e4ef4d7fdd55e5bf2de568836a2ab6d930
unknown <njorogekamau63@gmail.com>
2023-02-13 09:26:15 +0300

    I added a participant attribute to the Matter model

--- base/models.py
+++ base/models.py
@@ -44,6 +44,7 @@ class Matter(models.Model):
     close_date = models.DateField(blank=True, null=True)
     original_lawyer = models.ForeignKey(Lawyer, related_name='original_lawyer', on_delete=models.CASCADE)
     current_lawyer = models.ForeignKey(Lawyer, related_name='current_lawyer', on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User, blank=True)
     private = models.BooleanField(default=False)
     client = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
