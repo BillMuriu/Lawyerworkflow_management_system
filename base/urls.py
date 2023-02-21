17e94b9a7e76816741657ed6c7b3172c77b04cf1
unknown <njorogekamau63@gmail.com>
2023-02-21 15:42:46 +0300

    The 1st iteration of the update_matter functon

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('matters/', views.matters, name='matters'),
     path('matters/create_matter', views.create_matter, name='create_matter'),
     path('matter_detail/<int:pk>/', views.matter_detail, name='matter_detail'),
+    path('matters/update_matter/<int:pk>/', views.update_matter, name='update_matter'),
     path('matter_detail/<int:pk>/tasks/', views.matter_tasks, name='matter_tasks'),
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
