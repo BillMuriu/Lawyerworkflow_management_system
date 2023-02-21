016e76b210e42851b08e94b772d38b3bbee8575e
unknown <njorogekamau63@gmail.com>
2023-02-21 16:10:50 +0300

    Iteration 1 of the delete matter function

--- base/urls.py
+++ base/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('matters/create_matter', views.create_matter, name='create_matter'),
     path('matter_detail/<int:pk>/', views.matter_detail, name='matter_detail'),
     path('matters/update_matter/<int:pk>/', views.update_matter, name='update_matter'),
+    path('matters/delete_matter/<int:matter_id>/', views.delete_matter, name='delete_matter'),
     path('matter_detail/<int:pk>/tasks/', views.matter_tasks, name='matter_tasks'),
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
